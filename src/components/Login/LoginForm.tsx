fa1dbd90244fda02f13e210b3c70706426eb32cc
kybaq <flfsk12@gmail.com>
2025-02-13 20:51:01 +0900

    feat: Login 페이지 추가

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,29 @@
+const LoginForm = () => {
+  return (
+    <form onSubmit={(evt) => void handleSubmit(evt)}>
+      <label htmlFor="ID">아이디: </label>
+      <input type="text" name="user_id" id="ID" value={userId} onChange={(evt) => setUserId(evt.target.value)} />
+      <label htmlFor="PASSWORD">비밀번호: </label>
+      <input
+        type="password"
+        name="user_pw"
+        id="PASSWORD"
+        value={userPw}
+        onChange={(evt) => setUserPw(evt.target.value)}
+      />
+      <label htmlFor="NICKNAME">닉네임: </label>
+      <input
+        type="text"
+        name="user_nickname"
+        id="NICKNAME"
+        value={userNickname}
+        onChange={(evt) => setUserNickname(evt.target.value)}
+      />
+      <button type="submit" className="border-2">
+        제출
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
