de14c59d0ab6624f4afa1f5600e944203878904b
kybaq <flfsk12@gmail.com>
2025-02-13 20:52:41 +0900

    feat: Login 페이지 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,46 @@
+import { FormEvent, useState } from 'react';
+import { axiosInstance } from '../../services/axios';
+import { Link } from 'react-router';
+import { LoginResponse } from '../../types/users/Users.types';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [userId, setUserId] = useState<string>('');
+  const [userPw, setUserPw] = useState<string>('');
+
+  const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    try {
+      const response = await axiosInstance.post<LoginResponse>('/login', {
+        id: userId,
+        password: userPw,
+      });
+      console.log(response.data.success);
+    } catch (error) {
+      console.error('회원가입에 문제가 발생했습니다.', error);
+    }
+  };
+
+  return (
+    <>
+      <Link to="/">메인페이지로 이동</Link>
+      <form onSubmit={(evt) => void handleSubmit(evt)}>
+        <label htmlFor="ID">아이디: </label>
+        <input type="text" name="user_id" id="ID" value={userId} onChange={(evt) => setUserId(evt.target.value)} />
+        <label htmlFor="PASSWORD">비밀번호: </label>
+        <input
+          type="password"
+          name="user_pw"
+          id="PASSWORD"
+          value={userPw}
+          onChange={(evt) => setUserPw(evt.target.value)}
+        />
+        <button type="submit" className="border-2">
+          제출
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
