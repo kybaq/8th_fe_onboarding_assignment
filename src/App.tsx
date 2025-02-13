4a11013aa96c7f4d3a00b9e1e819b5b3168a8e55
kybaq <flfsk12@gmail.com>
2025-02-13 17:11:10 +0900

    feat: Post / Signup 반환값 타입 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import { useQuery } from '@tanstack/react-query';
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  completed: false;
+}
 
 export default function App() {
+  const { isPending, error, data } = useQuery<Post[]>({
+    queryKey: ['data'],
+    queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((response) => response.json()),
+  });
+
+  if (isPending) return <div>데이터를 요청하는 중...</div>;
+  else if (error) return <div>데이터를 가져오는 중 오류가 발생했습니다.</div>;
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>{data.map((el) => el.title)}</div>
       <Outlet />
     </>
   );
