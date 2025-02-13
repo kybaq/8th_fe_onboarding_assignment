cec6571787f35f23c459ba3b4ef62c82eb359128
kybaq <flfsk12@gmail.com>
2025-02-13 16:59:41 +0900

    feat: signup Route 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import SignUp from './components/SignUp/SignUp.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
