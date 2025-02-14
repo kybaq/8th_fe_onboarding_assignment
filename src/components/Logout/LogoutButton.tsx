5059fb309089c902121c0d2d07b94ba666f80eb9
kybaq <flfsk12@gmail.com>
2025-02-14 18:03:50 +0900

    chore: 로그인 상태에 따라 UI 나타나지 않도록 조절

--- src/components/Logout/LogoutButton.tsx
+++ src/components/Logout/LogoutButton.tsx
@@ -1,9 +1,15 @@
 import useLogout from '../../hooks/Login/useLogout';
+import useAuthStore from '../../stores/useAuth';
 
 const LogoutButton = () => {
+  const accessToken = useAuthStore((state) => state.accessToken);
   const { handleLogout } = useLogout();
 
-  return <button onClick={handleLogout}>로그아웃</button>;
+  return (
+    <button className={`underline ${accessToken ? 'block' : 'hidden'}`} onClick={handleLogout}>
+      로그아웃
+    </button>
+  );
 };
 
 export default LogoutButton;
